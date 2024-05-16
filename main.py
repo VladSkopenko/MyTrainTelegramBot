9c913e12c2abe74b3373e9064e258745d12bb096
VladSkopenko <skopirka2k17@gmail.com>
2024-05-16 16:26:56 +0300

    first start complete

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
 import asyncio
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+token = os.getenv('TOKEN')
+admin_id = os.getenv('ADMIN_ID')
+
+bot = Bot(token=token, default=DefaultBotProperties(parse_mode='HTML'))
+dp = Dispatcher()
+
+
+async def start_bot(bot: Bot):
+    await bot.send_message(admin_id, 'bot running')
+
+
+dp.startup.register(start_bot)
+
+
+async def start():
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(start())
