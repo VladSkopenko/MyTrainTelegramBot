6ea7c28d5b1aa1c40c52782efc12799c7fd2ec1b
VladSkopenko <skopirka2k17@gmail.com>
2024-05-17 17:02:46 +0300

    start part finish

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -4,7 +4,6 @@ from aiogram.types import CallbackQuery
 from aiogram.types import Message
 
 from src.key_boadrs.inline import get_callback_buttons
-from src.key_boadrs.reply_bilder import get_keyboard
 from src.key_boadrs.register_key_boards import register_keyboard
 
 USERS_LANGS = {}
@@ -16,7 +15,8 @@ async def get_start_and_choice_lang(message: Message, bot: Bot, ):
                "русский": "русский", }
     keyboard_markup = get_callback_buttons(buttons=buttons, sizes=(2,))
 
-    await bot.send_message(message.from_user.id, "Оберіть мову:", reply_markup=keyboard_markup)
+    await bot.send_message(message.from_user.id, "Оберіть мову:",
+                           reply_markup=keyboard_markup)
 
 
 async def language_callback(callback_query: CallbackQuery):
@@ -28,10 +28,7 @@ async def language_callback(callback_query: CallbackQuery):
     else:
         lang_message = f"{callback_query.from_user.first_name}, выбран {lang} язык"
     await callback_query.message.answer(f"{lang_message}")
-    await callback_query.message.answer(f"{callback_query.from_user.first_name}, Авторизуйтесь будь-ласка", reply_markup=register_keyboard)
+    await callback_query.message.answer(f"{callback_query.from_user.first_name}, Авторизуйтесь будь-ласка",
+                                        reply_markup=register_keyboard)
 
 
-
-# async def get_auth_or_register(message: Message, bot: Bot):
-#     await bot.send_message(message.chat.id, "Пожалуйста авторизуйтесь",
-#                            reply_markup=register_keyboard())
